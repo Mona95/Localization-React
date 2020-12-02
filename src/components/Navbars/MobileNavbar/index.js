43f9462a0a4ed3f15362e1395ca03c64a7a7cbb8
shamona9594 <shamona9594@gmail.com>
2020-12-02 22:43:03 +0300

    update mobileNavbar items

--- src/components/Navbars/MobileNavbar/index.js
+++ src/components/Navbars/MobileNavbar/index.js
@@ -1,31 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
+import NestedMenuItem from "material-ui-nested-menu-item";
 
-export default function MobileNavbar(props) {
-  const isMobileMenuOpen = Boolean(props.mobileMoreAnchorEl);
-  const mobileMenu = (
-    <Menu
-      anchorEl={props.mobileMoreAnchorEl}
-      anchorOrigin={{ vertical: "top", horizontal: "right" }}
-      id={props.mobileMenuId}
-      keepMounted
-      transformOrigin={{ vertical: "top", horizontal: "right" }}
-      open={isMobileMenuOpen}
-      name="mobileMoreAnchorEl"
-      onClose={props.handleClose}
-    >
-      <MenuItem onClick={props.handleClose}>
-        <p>Messages</p>
-      </MenuItem>
-      <MenuItem>
-        <p>Notifications</p>
-      </MenuItem>
-      <MenuItem>
-        <p>Profile</p>
-      </MenuItem>
-    </Menu>
-  );
+class MobileNavbar extends Component {
+  render() {
+    const isMobileMenuOpen = Boolean(this.props.mobileMoreAnchorEl);
+    const mobileMenu = (
+      <Menu
+        anchorEl={this.props.mobileMoreAnchorEl}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        id={this.props.mobileMenuId}
+        keepMounted
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
+        open={isMobileMenuOpen}
+        name="mobileMoreAnchorEl"
+        onClose={this.props.handleClose}
+      >
+        <MenuItem onClick={this.props.handleClose}>
+          <p>HOME</p>
+        </MenuItem>
+        <MenuItem onClick={this.props.handleClose}>
+          <p>CONTACT US</p>
+        </MenuItem>
+        <NestedMenuItem
+          label="EN"
+          parentMenuOpen={!!isMobileMenuOpen}
+          onClick={this.handleItemClick}
+        >
+          <MenuItem>EN</MenuItem>
+          <MenuItem>TR</MenuItem>
+        </NestedMenuItem>
+        <MenuItem>
+          <p>LOGIN</p>
+        </MenuItem>
+        <NestedMenuItem label="User name" parentMenuOpen={!!isMobileMenuOpen}>
+          <MenuItem>user email</MenuItem>
+          <MenuItem>Log out</MenuItem>
+        </NestedMenuItem>
+      </Menu>
+    );
 
-  return mobileMenu;
+    return mobileMenu;
+  }
 }
+
+export default MobileNavbar;
